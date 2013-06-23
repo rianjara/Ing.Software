94a42031d150d91c81eb0359d37b7d147627fcef
Aníbal <Anbal@ANIBAL>
2013-06-22 21:59:14 -0500

    ##Se movieron los métodos de WebApplicatio.view a moduloClientes.view para mantener concepto de interfaces por componente

--- WebApplication/urls.py
+++ WebApplication/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from WebApplication.views import *
+from moduloClientes.views import buscar_form, clientes, buscar_cliente,\
+    nuevo_cliente
+from WebApplication.views import current_datetime, hello
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -16,14 +18,5 @@ urlpatterns = patterns('',
                        (r'^search/$', buscar_cliente),
                        url(r'^nuevoCliente/$', nuevo_cliente),
                        url(r'^nuevo_cliente/$', nuevo_cliente),
-    # Examples:
-    # url(r'^$', 'WebApplication.views.home', name='home'),
-    #url(r'^WebApplication/', include('moduloClientes.models')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
-    (r'^admin/', include(admin.site.urls)),
+                       (r'^admin/', include(admin.site.urls)),
 )
