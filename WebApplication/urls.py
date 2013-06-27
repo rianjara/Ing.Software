43ef8afc255ee8303c8a7dcbef22ee6f966d16e8
rianjara <rianjara@espol.edu.ec>
2013-06-27 14:17:58 -0500

    ulti rianjara

--- WebApplication/urls.py
+++ WebApplication/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 from moduloClientes.views import buscar_form, clientes, buscar_cliente,\
     nuevo_cliente, editar_cliente, eliminar_cliente
-from moduloInventario.views import inventario
+from moduloInventario.views import inventario, nuevo_item, editar_item, eliminar_item
 from WebApplication.views import current_datetime, hello
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,6 +15,9 @@ urlpatterns = patterns('',
                        url(r'^hello/$', hello),
                        url(r'^clientes/$', clientes),
                        url(r'^inventario/$', inventario),
+                       url(r'^nuevoItem/$', nuevo_item),
+                       url(r'^editarItem/$', editar_item),
+                       url(r'^eliminarItem/$', eliminar_item),
                        #url(r'^client/(\d+)/$', 'view.buscar_cliente'),
                        url(r'^busquedaClientes/$', buscar_form),
                        (r'^search/$', buscar_cliente),
