e613b5f7841c5f4e2fed36496dcf24e98f31edda
Brick Reyes Zambrano <brickrey@hotmail.com>
2013-08-21 01:33:23 -0500

    Act 21 agosto

--- WebApplication/urls.py
+++ WebApplication/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import *
 from django.contrib import admin
 from moduloClientes.views import buscar_form, clientes, buscar_cliente,\
     nuevo_cliente, editar_cliente, eliminar_cliente
-from moduloInventario.views import nuevo_item, inventario, editar_item, ordenes_compras, nueva_orden_compra, editar_orden_compra, nuevo_detalle_compra, editar_detalle_compra
+from moduloInventario.views import nuevo_item, inventario, editar_item, ordenes_compras, nueva_orden_compra, editar_orden_compra, nuevo_detalle_compra, editar_detalle_compra, proveedores, nuevo_proveedor, eliminar_detalle_compra, editar_proveedor, categorias, nueva_categoria, editar_categoria, detalles_compra
 from WebApplication.views import current_datetime, hello, index, error404
 
 # Uncomment the next two lines to enable the admin:
@@ -23,14 +23,22 @@ urlpatterns = patterns('',
                        url(r'^editarCliente/$', editar_cliente),
                        url(r'^eliminarCliente/$', eliminar_cliente),
                        url(r'^index/$', index),
+                       url(r'^proveedores/$',proveedores),
+                       url(r'^nuevoProveedor/$',nuevo_proveedor),
+                       url(r'^editarProveedor/$',editar_proveedor),
+                       url(r'^categorias/$',categorias),
+                       url(r'^nuevaCategoria/$',nueva_categoria),
+                       url(r'^editarCategoria/$',editar_categoria),
                        url(r'^inventario/$', inventario),
                        url(r'^nuevoItem/$', nuevo_item),
                        url(r'^editarItem/$', editar_item),
                        url(r'^ordenesCompra/$',ordenes_compras),
                        url(r'^nuevaOrdenCompra/$',nueva_orden_compra),
                        url(r'^editarOrdenCompra/$',editar_orden_compra),
+                       url(r'^detallesCompra/$',detalles_compra),
                        url(r'^nuevoDetalleCompra/$',nuevo_detalle_compra),
                        url(r'^editarDetalleCompra/$',editar_detalle_compra),
+                       url(r'^eliminarDetalleCompra/$',eliminar_detalle_compra),
                        (r'^admin/', include(admin.site.urls)),
                        url(r'^$', index),
 )
